0f621cc2107ab7e0908a5882ad765c2377ec15e8
Taka200 <tncyberport@gmail.com>
2019-02-06 13:21:00 +0800

    showing picture

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let pictureArray = ["picture1", "picture2", "picture3", "picture4", "picture5"]
+    var slideShow: Int = 0
+   
+    @IBOutlet weak var slideShowImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func returnButton(_ sender: UIButton) {
+        if slideShow != 0 {
+            slideShow -= 1
+        } else {
+            slideShow = 4
+        }
+        slideShowImage.image = UIImage(named: pictureArray[slideShow])
+    }
+    @IBAction func playButton(_ sender: UIButton) {
+        slideShowImage.image = UIImage(named: pictureArray[slideShow])
+    }
+    @IBAction func forwardButton(_ sender: UIButton) {
+        if slideShow != 4{
+            slideShow += 1
+        } else {
+            slideShow = 0
+        }
+        slideShowImage.image = UIImage(named: pictureArray[slideShow])
+    }
+    
+    
+    
+    
 }
 
