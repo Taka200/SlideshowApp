78be888a487af3e639ee6a83f181e68f62754b67
Taka200 <tncyberport@gmail.com>
2019-02-08 03:37:47 +0800

    課題5の要件8まで

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     let pictureArray = ["picture1", "picture2", "picture3", "picture4", "picture5"]
     var slideShow: Int = 0
+    var timer: Timer!
    
 
     @IBOutlet weak var slideShowImage: UIImageView!
@@ -20,8 +21,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
+    //呼び出されるタイマー関数
+    @objc func updateTimer(_ timer: Timer) {
+        if slideShow != 4{
+            slideShow += 1
+        } else {
+            slideShow = 0
+        }
+        slideShowImage.image = UIImage(named: pictureArray[slideShow])
+    }
+    
+    
+    
     @IBAction func returnButton(_ sender: UIButton) {
         if slideShow != 0 {
             slideShow -= 1
@@ -31,8 +45,16 @@ class ViewController: UIViewController {
         slideShowImage.image = UIImage(named: pictureArray[slideShow])
     }
     @IBAction func playButton(_ sender: UIButton) {
-        slideShowImage.image = UIImage(named: pictureArray[slideShow])
+        //タイマーの作成・始動
+        if timer == nil {
+            Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+        } else {
+            timer.invalidate()
+            timer = nil
+        }
     }
+    
+    
     @IBAction func forwardButton(_ sender: UIButton) {
         if slideShow != 4{
             slideShow += 1
